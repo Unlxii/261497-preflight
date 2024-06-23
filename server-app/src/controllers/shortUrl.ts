7b007563d97e4381bea8c09cf5f7336a45a46797
tent <tteenntt.2003@hotmail.com>
2024-06-23 11:04:33 +0700

    sever-app done

---
+++ server-app/src/controllers/shortUrl.ts
@@ -0,0 +1,61 @@
+import  express  from "express";
+import { urlModel } from '../model/shortUrl';
+import { url } from "inspector";
+import e from "express";
+
+export const createUrl = async (req: express.Request, res: express.Response) => {
+    try{
+        console.log("The fullUrl is:", req.body.fullUrl);
+        const {fullUrl} = req.body;
+        const urlfound = await urlModel.find({fullUrl});
+        if(urlfound.length > 0){
+            res.status(409);
+            res.send(urlfound);
+        }else {
+            const shortUrl = await urlModel.create({fullUrl});
+            res.status(201).send(shortUrl);
+        }
+    }catch(error){
+        res.status(500).send({message: "Something went wrong!"});
+    }
+};
+export const getAllUrl = async (req: express.Request, res: express.Response) => {
+    try{
+        const shortUrls = await urlModel.find()
+        if(shortUrls.length < 0){
+            res.status(400).send("shortUrl not found");
+    }else{
+        res.status(200).send(shortUrls);
+    }
+}catch(error){
+    res.status(500).send({message: "Something went wrong!"});
+    }}
+export const getUrl = async (req: express.Request, res: express.Response) => {
+   
+    try{
+        const shortUrl = await urlModel.findOne({shortUrl: req.params.id});
+        if(!shortUrl){
+            res.status(404).send("Full ShortUrl not found!");
+        }else{
+            shortUrl.click ++;
+            shortUrl.save();
+            res.redirect(`${shortUrl.fullUrl}`)
+        }
+    }catch(error){
+        res.status(500).send({message: "Something went wrong!"});
+    }
+   
+   
+    
+};
+export const deleteUrl = async (req: express.Request, res: express.Response) => {
+    try{
+        const shortUrl = await urlModel.findByIdAndDelete({_id: req.params.id});
+        if(shortUrl){
+            res.status(200).send("Request URL deleted successfully");
+        }
+    }catch(error){
+        res.status(500).send({message: "Something went wrong!"});
+    }
+   
+};
--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
+import  connectDB  from './config/dbConfig';
+import shortUrl from './routes/shortUrl';
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api/", shortUrl);
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the URL Shortener API");
+
+// });
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
