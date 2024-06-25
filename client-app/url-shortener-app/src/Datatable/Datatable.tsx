a6e1afe18e1b4c383c8b7b41075ec990320de0b1
tteenntt <87744652+Unlxii@users.noreply.github.com>
2024-06-25 09:35:08 +0700

    added delete and copy but not realtime

--- client-app/url-shortener-app/src/Datatable/Datatable.tsx
+++ client-app/url-shortener-app/src/Datatable/Datatable.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { UrlData } from "../interface/UrlData";
 import { Link } from "react-router-dom";
 import { ServerUrl } from "../helper/Constants";
+import axios from "axios";
 
 interface IDataTableProps {
   data: UrlData[];
@@ -29,7 +30,10 @@ const DataTable: React.FunctionComponent<IDataTableProps> = (props) => {
           <td className="px-6 py-3">{item.shortUrl}</td>
           <td className=" px-96 py-3">
             <div className="flex-content-center">
-              <div className="cursor-pointer px-2">
+              <div
+                className="cursor-pointer px-2"
+                onClick={() => copyToClipboard(item.shortUrl)}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -45,29 +49,48 @@ const DataTable: React.FunctionComponent<IDataTableProps> = (props) => {
                   />
                 </svg>
               </div>
-            </div>
-            <div className="flex-content-center">
-              <div className="cursor-pointer px-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="size-6"
+              <div className="flex-content-center">
+                <div
+                  className="cursor-pointer px-2"
+                  onClick={() => deleteUrl(item._id)}
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 fill-red-500 hover:fill-red-600"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </td>
         </tr>
       );
     });
+  };
+  const copyToClipboard = async (url: string) => {
+    try {
+      await navigator.clipboard.writeText(`${ServerUrl}/shortUrl/${url}`);
+      alert(`Copied to clipboard :${ServerUrl}/shortUrl/${url} `);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deleteUrl = async (id: string) => {
+    try {
+      const response = await axios.delete(`${ServerUrl}/shortUrl/${id}`);
+      console.log(response, `is deletedsuccessfully`);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="container mx-auto pt-2 pb-10">
