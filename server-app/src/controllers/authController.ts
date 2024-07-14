100e2a995084e254c8112e0d89b1d3f287228279
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-14 19:48:17 +0700

    Add authController for user registration

---
+++ server-app/src/controllers/authController.ts
@@ -0,0 +1,34 @@
+const User = require("../model/user");
+
+import { Request, Response } from "express";
+
+const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+    // Check name
+    if (!name) {
+      return res.json({
+        error: "name is required",
+      });
+    }
+    // Check pwd is good
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and should be at least 6 characters long",
+      });
+    }
+    // Check email
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is already taken",
+      });
+    }
+    const user = await User.create({ name, email, password });
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = registerUser;
