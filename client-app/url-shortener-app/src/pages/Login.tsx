c14a47e433d39d7a7ef6e1406e7e4bfad8f1fb42
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-14 21:16:48 +0700

    Implement login page

--- client-app/url-shortener-app/src/pages/Login.tsx
+++ client-app/url-shortener-app/src/pages/Login.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [data, setData] = useState({
     email: "",
     password: "",
   });
 
-  const loginUser = (e: React.FormEvent<HTMLFormElement>) => {
+  const loginUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios.get("/");
+    const { email, password } = data;
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5001/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      if (data.error) {
+        toast.error(data.error);
+      } else {
+        setData({
+          email: "",
+          password: "",
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
