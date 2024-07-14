7c1441cd1464b842dcc17d579f791d66dd8c1c8f
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-14 19:02:48 +0700

    Add routes for login and register

--- client-app/url-shortener-app/src/App.tsx
+++ client-app/url-shortener-app/src/App.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 import Container from "./component/Container/Container";
+import Register from "./pages/Regiser";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 interface IAppProps {}
 
@@ -9,7 +13,12 @@ const App: React.FunctionComponent<IAppProps> = () => {
   return (
     <>
       <Header />
-      <Container />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/main" element={<Container />}></Route>
+      </Routes>
       <Footer />
     </>
   );
