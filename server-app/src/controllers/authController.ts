c896a63943e61c03754737a7c45b3fe69dafdd38
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-14 21:17:27 +0700

    Fix authController

--- server-app/src/controllers/authController.ts
+++ server-app/src/controllers/authController.ts
@@ -1,8 +1,11 @@
 const User = require("../model/user");
 const { hashPassword, comparePassword } = require("../helpers/auth");
+import { error } from "console";
+import { defaultMaxListeners } from "events";
 import { Request, Response } from "express";
 
-const registerUser = async (req: Request, res: Response) => {
+// register endpoint
+export const registerUser = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
     // Check name
@@ -25,6 +28,7 @@ const registerUser = async (req: Request, res: Response) => {
       });
     }
     const hashedPassword = await hashPassword(password);
+    // create user in DB
     const user = await User.create({ name, email, password: hashedPassword });
     return res.json(user);
   } catch (error) {
@@ -32,4 +36,24 @@ const registerUser = async (req: Request, res: Response) => {
   }
 };
 
-export default registerUser;
+// login endpoint
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
