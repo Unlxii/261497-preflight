5c56eba5c096c10ee5750c581b16e8e96462cf55
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-14 22:42:02 +0700

    Add Get profile route

--- server-app/src/routes/authRoutes.ts
+++ server-app/src/routes/authRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController";
+import {
+  registerUser,
+  loginUser,
+  getProfile,
+} from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", getProfile);
 export default router;
