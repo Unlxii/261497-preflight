d8c6439591b2c4f612954e55cd6792726ba28d9a
Exzq_ <pariwat_wong@cmu.ac.th>
2024-07-16 21:53:07 +0700

    *

--- server-app/src/routes/shortUrl.ts
+++ server-app/src/routes/shortUrl.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { createUrl } from '../controllers/shortUrl';
-import { deleteUrl, getAllUrl, getUrl } from '../controllers/shortUrl';
+import { createUrl, deleteUrl, getAllUrl, getUrl } from '../controllers/shortUrl'; // Import all functions
 import { getUserUrls } from '../controllers/user';
 
 const router = express.Router();
 
 router.post("/shortUrl", createUrl);
-router.get("/shortUrl", getAllUrl);
+router.get("/shortUrl", getAllUrl); //for admin
+router.get("/user/:userId/shortUrls", getUserUrls); 
 router.get("/shortUrl/:id", getUrl);
 router.delete("/shortUrl/:id", deleteUrl);
 
