6152492a8a1087869d12bde9f0612d9447fef551
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-16 10:11:59 +0700

    Refactor ShortUrl

--- server-app/src/routes/shortUrl.ts
+++ server-app/src/routes/shortUrl.ts
@@ -1,13 +1,11 @@
-import exp from 'constants';
-import { create } from 'domain';
 import express from 'express';
 import { createUrl, deleteUrl, getAllUrl, getUrl } from '../controllers/shortUrl';
 
 const router = express.Router();
 
-router.post("/shortUrl", createUrl);
-router.get("/shortUrl", getAllUrl);
-router.get("/shortUrl/:id", getUrl);
-router.delete("/shortUrl/:id", deleteUrl);
+router.post('/shortUrl', createUrl);
+router.get('/shortUrl', getAllUrl);
+router.get('/shortUrl/:id', getUrl);
+router.delete('/shortUrl/:id', deleteUrl);
 
 export default router;
