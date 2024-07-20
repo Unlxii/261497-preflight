d48542d90478bb374848b881338934a94dd34835
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-20 16:48:09 +0700

    Remove import register

--- client-app/url-shortener-app/src/pages/Login.tsx
+++ client-app/url-shortener-app/src/pages/Login.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import Register from "./Register";
 
 const Login = () => {
   const navigate = useNavigate();
