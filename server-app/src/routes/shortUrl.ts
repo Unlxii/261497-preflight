466dd3fbed7526410214b400fb47514ca00ff26f
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-26 02:29:00 +0700

    Refactor shortUrl route file

--- server-app/src/routes/shortUrl.ts
+++ server-app/src/routes/shortUrl.ts
@@ -1,14 +1,17 @@
-import express from 'express';
-import { createUrl, deleteUrl, getAllUrl, getShortUrlsByUserId, getUrl } from '../controllers/shortUrl'; // Import all functions
-import { getUserUrls } from '../controllers/user';
+import express from "express";
+import {
+  createUrl,
+  deleteUrl,
+  getAllUrl,
+  getUrl,
+} from "../controllers/shortUrl"; // Import all functions
+import { getUserUrls } from "../controllers/user";
 
 const router = express.Router();
 
 router.post("/shortUrl", createUrl);
 router.get("/shortUrl", getAllUrl); // Fetch all short URLs (admin)
-router.get("/public/user/:userId/shortUrls", getShortUrlsByUserId);
 router.get("/shortUrl/:id", getUrl);
 router.delete("/shortUrl/:id", deleteUrl);
-
 
 export default router;
