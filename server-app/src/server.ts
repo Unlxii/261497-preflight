830198be0ac1c4dad2916e59a3aee352e4da500b
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-26 22:18:59 +0700

    Update server.ts to allow CORS

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -23,7 +23,9 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:3001",
-      "http://localhost:5173",
+      "http://localhost:5000",
+      "http://localhost:5001",
+      "http://localhost:27017",
     ],
     credentials: true,
   })
