f3d23e663ca04906422200832f0180b5f34c7c5c
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-26 17:41:09 +0700

    Remove unused PullingTest component

--- client-app/url-shortener-app/src/App.tsx
+++ client-app/url-shortener-app/src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import PullingTest from "./pages/pullingtest"; // Import your PullingTest component
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { UserContextProvider } from "./context/userContext";
@@ -12,8 +11,6 @@ import ProtectedRoute from "./components/ProtectedRoutes";
 interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = () => {
-  
-
   return (
     <UserContextProvider>
       <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
@@ -29,8 +26,6 @@ const App: React.FunctionComponent<IAppProps> = () => {
             </ProtectedRoute>
           }
         ></Route>
-        <Route path="/user/:userId/shortUrls" element={<PullingTest userId="6695ede207e88cf892760d8f" />} />
-
       </Routes>
     </UserContextProvider>
   );
