036b7fb491aaa91a32a172230c2420d9af17a2ab
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-26 17:44:33 +0700

    Update CORS configuration

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -20,13 +20,17 @@ app.use(express.urlencoded({ extended: false }));
 // Updated CORS configuration to allow localhost:3000 AND localhost:3001
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
 
 // Routes
-app.use("/api", shortUrl); 
+app.use("/api", shortUrl);
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
