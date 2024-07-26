fa8eb66a9f72ddca16f5056801a8451680958abb
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-07-26 17:44:48 +0700

    Update MongoDB connection

--- server-app/src/config/dbConfig.ts
+++ server-app/src/config/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(`${process.env.CONNECTION_STRING}`);
+    const connect = await mongoose.connect(`mongodb://localhost:27017/shorterURL`);
     console.log(
       "MongoDB Connected:",
       connect.connection.host,
